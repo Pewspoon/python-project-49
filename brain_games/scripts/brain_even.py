f4e48f32c3bdd9432ed1378224585472a8653a1d
Stepan Kozlov <pewspoon.info@gmail.com>
2024-01-02 13:13:44 +0300

    5 step changes

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,25 +1,25 @@
 from random import randint
-from brain_games.cli import name
 import prompt
 
 
-numbers = randint(0, 100)
-answer = 'yes' if numbers % 2 == 0 else 'no'
-result = 0
 def question_for_user():
+    numbers = randint(0, 100)
+    answer = 'yes' if numbers % 2 == 0 else 'no'
+    result = 0
+    
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    print(f"Question: {numbers}")
-    user_answer = prompt.string('Your answer:')
-
-    while(result != 3):
+    while (result != 3):
+        print(f"Question: {numbers}")
+        user_answer = prompt.string('Your answer:')
+        
         if user_answer == answer:
             print('Correct!')
             result += 1
         else:
             print('\'yes\' is wrong answer ;(. Correct answer was \'no\'.')
-            print(f"Let\'s try again, {name}!")
+            print(f"Let\'s try again, !")
             result = 0
-    print(f"Congratulations, {name}!")
+    print(f"Congratulations, !")
 
 
 def main():
