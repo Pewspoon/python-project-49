97da5466d1b91ad53f9a9163c914b4a6cb7728ef
Stepan Kozlov <pewspoon.info@gmail.com>
2024-01-03 18:17:43 +0300

    9 step changes

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 https://asciinema.org/a/wwF6Sb9WcU2kOK5y4O9ZoEHnp - brain-even
 https://asciinema.org/a/EwQ1IfR8ZEpehMKK8CQ2g3tli - brain-calc
 https://asciinema.org/a/1OagKOQiOVYoqHuYVNM3QG3QW - brain-gcd
+https://asciinema.org/a/NeZrctYQmexHQJyoTQxkBt1sq - brain-progression
+https://asciinema.org/a/eweGPupW8z3R5ZLUy6o8pdcKp - brain-prime 
 [![Actions Status](https://github.com/Pewspoon/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Pewspoon/python-project-49/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/42c86a4969edd0e7b9c9/maintainability)](https://codeclimate.com/github/Pewspoon/python-project-49/maintainability)
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,46 @@
+import random
+from random import randint
+import prompt
+
+
+
+def question_for_user():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f"Hello, {name}!")
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    result = 0
+    while result != 3:
+        number = randint(0, 1001)
+        print(f"Question: {number}")
+        user_answer = prompt.string("Your answer:")
+        if user_answer.lower() == check_numbers_for_simple(number):
+            print("Correct!")
+            result += 1
+        elif user_answer == 'yes' and check_numbers_for_simple(number) == 'no':
+            print('\'yes\' is wrong answer ;(. Correct answer was \'no\'.')
+            print(f"Let\'s try again, {name}!")
+            result = 0
+        else:
+            print('\'no\' is wrong answer ;(. Correct answer was \'yes\'.')
+            print(f"Let\'s try again, {name}!")
+            result = 0
+    print(f"Congratulations, {name}!")
+
+
+def check_numbers_for_simple(number):
+    if number == 1:
+        return 'no'
+    for i in range(2, number):
+        if number % i == 0:
+            return 'no'
+    return 'yes'
+    
+
+
+def main():
+    question_for_user()
+
+
+if __name__ == '__main__':
+    main()
