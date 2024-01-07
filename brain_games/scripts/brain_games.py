52cfc15c1b6ae83ca84761ef3aad6b04f2951e77
Stepan Kozlov <pewspoon.info@gmail.com>
2024-01-07 21:39:38 +0300

    first project changes

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -5,3 +5,11 @@ from brain_games.cli import welcome_user
 def welcome():
     print('Welcome to the Brain Games!')
     welcome_user()
+
+
+def main():
+    welcome()
+
+
+if __name__ == '__main__':
+    main()
